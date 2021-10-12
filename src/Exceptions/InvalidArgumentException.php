42ad42e2b4ed7709db1e2dfee8bd21f2a926a72b
wangwei <wangwei@fanxiapp.com>
2021-10-12 03:30:56 +0000

    first commit

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Overtrue\Weather\Exceptions;
+
+class InvalidArgumentException extends \Exception
+{
+
+}
---
+++ src/Weather.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Overtrue\Weather;
+
+use GuzzleHttp\Client;
+use Overtrue\Weather\Exceptions\InvalidArgumentException;
+use Overtrue\Weather\Exceptions\HttpException;
+
+class Weather
+{
+    /**
+     * @var string
+     */
+    protected string $key;
+
+    /**
+     * @var array
+     */
+    protected array $guzzleOptions = [];
+
+    /**
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getHttpClient(): Client
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    /**
+     * @param        $city
+     * @param string $type
+     * @param string $format
+     *
+     * @return mixed|string
+     * @throws HttpException
+     * @throws InvalidArgumentException
+     */
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format);
+        }
+
+        if (!\in_array(\strtolower($type), ['base', 'all'])) {
+            throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
+        }
+
+        $query = array_filter([
+            'key'        => $this->key,
+            'city'       => $city,
+            'output'     => \strtolower($format),
+            'extensions' => \strtolower($type),
+        ]);
+
+        try {
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
+
+            return 'json' === $format ? \json_decode($response, true) : $response;
+        } catch (\Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
